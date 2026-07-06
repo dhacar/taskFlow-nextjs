68291383d6d02e95fe923e4aa2b56cb1aa2a2a49
sakariye mohamud ali <zakimaxamuudcali49@gmail.com>
2026-07-06 13:36:37 +0300

    v2

--- auth.config.ts
+++ auth.config.ts
@@ -9,12 +9,12 @@ export const authConfig = {
   },
   providers: [
     Google({
-      clientId: process.env.AUTH_GOOGLE_ID ?? "",
-      clientSecret: process.env.AUTH_GOOGLE_SECRET ?? ""
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
     }),
     GitHub({
-      clientId: process.env.AUTH_GITHUB_ID ?? "",
-      clientSecret: process.env.AUTH_GITHUB_SECRET ?? ""
+      clientId: process.env.GITHUB_CLIENT_ID ?? "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? ""
     })
   ],
   pages: {
